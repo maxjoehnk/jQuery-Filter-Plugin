113bfdee276b4f96be5e2e331f8dc2f00c8145b4
e-freak <maxjoehnk@gmail.com>
2013-10-16 12:56:37 +0000

    Build v1.0.1

--- dist/jquery.filter.js
+++ dist/jquery.filter.js
@@ -1,5 +1,5 @@
 /*!
-* jQuery Filter Plugin - v1.0.0 - 2013-10-10
+* jQuery Filter Plugin - v1.0.1 - 2013-10-16
 * http://projects.e-freak.org/jQuery-Filter-Plugin
 
 * Copyright (c) 2013 Max Joehnk;
@@ -8,42 +8,44 @@
 $(function() {
 	$('body').on('change', '.filter', function() {
 		var originalFilter = $(this);
-		$(originalFilter.attr('data-filter-container')).children('.filterable').each(function(index, filterable) {
-			var filters = $('body').find(".filter[data-filter-container='" + originalFilter.attr('data-filter-container') + "']");
-			var hide = false;
-			$.each(filters, function(i, element) {
-				var filter = $(element);
-				var filterVal = $(filterable).attr('data-' + $(filter).attr('data-filter')).toLowerCase();
-				if (filter.is('input')) {
-					if (filter.is('input[type=checkbox]')) {
-						if (filterVal !== filter.is(':checked').toString()) {
-							hide = true;
+		$(originalFilter.attr('data-filter-container')).each(function(i, container) {
+			$(container).find('.filterable').each(function(index, filterable) {
+				var filters = $('body').find(".filter[data-filter-container='" + originalFilter.attr('data-filter-container') + "']");
+				var hide = false;
+				$.each(filters, function(i, element) {
+					var filter = $(element);
+					var filterVal = $(filterable).attr('data-' + $(filter).attr('data-filter')).toLowerCase();
+					if (filter.is('input')) {
+						if (filter.is('input[type=checkbox]')) {
+							if (filterVal !== filter.is(':checked').toString()) {
+								hide = true;
+							}
+						}else {
+							if (filterVal.indexOf(filter.val().toLowerCase()) === -1 && filter.val() !== '') {
+								hide = true;
+							}
 						}
-					}else {
-						if (filterVal.indexOf(filter.val().toLowerCase()) === -1 && filter.val() !== '') {
-							hide = true;
+					}else if (filter.is('select')) {
+						if (filter.children(':selected').is('option[data-filter-ignore]')) {
+							return;
+						}
+						if (filter.children(':selected').is('option[data-filter-value]')) {
+							if (filterVal !== filter.children(':selected').attr('data-filter-value').toLowerCase()) {
+								hide = true;
+							}
+						}else {
+							if (filterVal !== filter.children(':selected').html().toLowerCase()) {
+								hide = true;
+							}
 						}
 					}
-				}else if (filter.is('select')) {
-					if (filter.children(':selected').is('option[data-filter-ignore]')) {
-						return;
-					}
-					if (filter.children(':selected').is('option[data-filter-value]')) {
-						if (filterVal !== filter.children(':selected').attr('data-filter-value').toLowerCase()) {
-							hide = true;
-						}
-					}else {
-						if (filterVal !== filter.children(':selected').html().toLowerCase()) {
-							hide = true;
-						}
-					}
+				});
+				if (hide) {
+					$(filterable).fadeOut();
+				}else {
+					$(filterable).fadeIn();
 				}
 			});
-			if (hide) {
-				$(filterable).fadeOut();
-			}else {
-				$(filterable).fadeIn();
-			}
 		});
 	});
 	
